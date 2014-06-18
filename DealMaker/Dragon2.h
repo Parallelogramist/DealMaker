798b31299163a6bd00626754046e4dfde420623b
georgy <georgy.golubev@gmail.com>
2014-06-18 13:40:36 -0700

    changes

---
+++ DealMaker/Dragon2.h
@@ -0,0 +1,13 @@
+//
+//  Dragon2.h
+//  DealMaker
+//
+//  Created by Georgy on 6/18/14.
+//  Copyright (c) 2014 Oscicoil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Dragon2 : UIViewController
+
+@end
