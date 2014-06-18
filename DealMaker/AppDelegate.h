b5edcb96586a3942d301dd81aeb81ac6244d349c
georgy <georgy.golubev@gmail.com>
2014-06-17 21:45:23 -0700

    Initial Commit

---
+++ DealMaker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DealMaker
+//
+//  Created by Georgy on 6/17/14.
+//  Copyright (c) 2014 Oscicoil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DealMaker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DealMaker
+//
+//  Created by Georgy on 6/17/14.
+//  Copyright (c) 2014 Oscicoil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
