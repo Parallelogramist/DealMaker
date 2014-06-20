3a3b7d2ff9f3b5751b700dd13286332b8ef6f708
georgy <georgy.golubev@gmail.com>
2014-06-19 20:02:39 -0700

    pass data between views, updated

--- DealMaker/DragonViewController.m
+++ DealMaker/DragonViewController.m
@@ -9,6 +9,7 @@
 #import "DragonViewController.h"
 
 @interface DragonViewController ()
+
 @end
 
 @implementation DragonViewController
@@ -26,9 +27,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    
-    self.label.text = self.text;
+
+    self.label.text = self.text;//add text from field to label
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,16 +36,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
